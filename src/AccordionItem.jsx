cc0eb045cd7c92f35ff11cfb2e46812e53de57a7
Angelapeters1601 <105676371+Angelapeters1601@users.noreply.github.com>
2024-07-14 20:52:54 +0200

    Update AccordionItem.jsx

--- src/AccordionItem.jsx
+++ src/AccordionItem.jsx
@@ -18,3 +18,6 @@ function AccordionItem({ num, title, text }) {
 }
 
 export default AccordionItem;
+// Initially, isOpen is false, so the content {text} is not rendered.
+//When the user clicks the item, handleToggle is called, toggling isOpen to true.
+//The component re-renders, and because isOpen is true, the content {text} is rendered.
