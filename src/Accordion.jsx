aaf7e6221744b5eb130735e7090f9c47de4d2f0f
Angelapeters1601 <nwattaha@gmail.com>
2024-07-19 15:29:21 +0200

    Lifting state and addng children prop

--- src/Accordion.jsx
+++ src/Accordion.jsx
@@ -1,11 +1,36 @@
+import React, { useState } from "react";
 import AccordionItem from "./AccordionItem";
 
 function Accordion({ faqs }) {
+  const [currOpen, setCurrOpen] = useState(null);
+
   return (
     <div className="accordion">
       {faqs.map((el, i) => (
-        <AccordionItem key={i} title={el.title} text={el.text} num={i} />
+        <AccordionItem
+          currOpen={currOpen}
+          setCurrOpen={setCurrOpen}
+          key={el.title}
+          title={el.title}
+          num={i}
+        >
+          {el.text}
+        </AccordionItem>
       ))}
+      <AccordionItem
+        currOpen={currOpen}
+        setCurrOpen={setCurrOpen}
+        key="test 1"
+        title="Thinking in React"
+        num={22}
+      >
+        <b>Allows React developers to:</b>
+        <ul>
+          <li>Break up UI into components</li>
+          <li>Make components reusable</li>
+          <li>Place state efficiently</li>
+        </ul>
+      </AccordionItem>
     </div>
   );
 }
--- src/AccordionItem.jsx
+++ src/AccordionItem.jsx
@@ -1,10 +1,8 @@
-import React, { useState } from "react";
-
-function AccordionItem({ num, title, text }) {
-  const [isOpen, setIsOpen] = useState(false);
+function AccordionItem({ num, title, currOpen, setCurrOpen, children }) {
+  const isOpen = num === currOpen;
 
   const handleToggle = () => {
-    setIsOpen((isOpen) => !isOpen);
+    setCurrOpen(isOpen ? null : num); // Toggle current item, close if already open
   };
 
   return (
@@ -12,9 +10,12 @@ function AccordionItem({ num, title, text }) {
       <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
       <p className="title">{title}</p>
       <p className="icon">{isOpen ? "- " : "+"}</p>
-      {isOpen && <div className="content-box">{text}</div>}
+      {isOpen && <div className="content-box">{children}</div>}
     </div>
   );
 }
 
 export default AccordionItem;
+// Initially, isOpen is false, so the content {text} is not rendered.
+//When the user clicks the item, handleToggle is called, toggling isOpen to true.
+//The component re-renders, and because isOpen is true, the content {text} is rendered.
